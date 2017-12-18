5e45371967ed4794bc5507f7d6d1a97731d2e0ac
Zenya Zelinsky <deimos3000@ukr.net>
2017-12-18 17:08:55 +0200

    hw done, 18.12.17

--- app/js/common.js
+++ app/js/common.js
@@ -70,21 +70,154 @@
 
 // lesson 4;
 
-var arr = [3,4,5,6,7,8];
-var f1 = function(x){
-	return x * 2;
+// var arr = [3,4,5,6,7,8];
+// var f1 = function(x){
+// 	return x * 2;
+// }
+
+// var map = function(fn, arr){
+// 	res = [];
+// 	for (var i = 0; i < arr.length; i++) {
+// 		res.push( fn(arr[i]));
+// 	}
+
+// 	return res;
+
+
+
+
+
+
+// homework
+
+
+// конструктор
+// function Animal() {
+//   this.name = "";
+//   this.speed = 0;
+// }
+
+// // методы в прототипе
+// Animal.prototype.run = function(speed) {
+//   this.speed += speed;
+//   alert('скорость ' + this.speed );
+// };
+
+// Animal.prototype.stop = function() {
+//   this.speed = 0;
+//   alert( this.name + ' стоит' );
+// };
+
+// var animal = new Animal();
+
+// console.log(animal(animal.run(5), animal.stop()));
+
+var findInArr = function(arr, val){
+	for (var i = 0; i <= arr.length; i++) {
+		if( arr[i] === val) {
+			return i;
+		}
+	return -1;
+	}
 }
 
-var map = function(fn, arr){
-	res = [];
-	for (var i = 0; i < arr.length; i++) {
-		res.push( fn(arr[i]));
+function Hamburger(size, stuffing) {
+	this._size = size;
+	this._stuffing = stuffing;
+	this._toppings = [];
+}
+
+
+
+	Hamburger.SIZE_SMALL = {
+		name: 'Small',
+		price: 50,
+		cal: 35
+	};
+	Hamburger.SIZE_LARGE = {
+		name: 'Large',
+		price: 60,
+		cal: 45
+	};
+	Hamburger.STUFFING_CHEESE = {
+		name: 'Cheese',
+		price: 10,
+		cal: 14
+	}
+	Hamburger.STUFFING_SALAD = {
+		name: 'Salad',
+		price: 8,
+		cal: 5
+	}
+	Hamburger.STUFFING_POTATO = {
+		name: 'Potato',
+		price: 8,
+		cal: 10
+	}
+	Hamburger.TOPPING_MAYO = {
+		name: 'Mayo',
+		price: 5,
+		cal: 171
+	}
+	Hamburger.TOPPING_SPICE = {
+		name: 'Spice',
+		price: 5,
+		cal: 2
 	}
 
-	return res;
+    Hamburger.prototype.addTopping = function (topping) {
+        var founded = findInArr(this._toppings, topping);
+        if(founded !== -1){
+            alert('Такой топпинг уже есть');
+        }else{
+            this._toppings.push(topping);
+        }
+    };
+
+	Hamburger.prototype.removeToping = function(topping){
+		var founded = findInArr(this._toppings, topping);
+		if(founded !== -1){
+			alert('Такой добавки нет');
+		} else {
+			this._toppings.splice(founded, 1);
+		}
+	};
+
+    Hamburger.prototype.getPrice = function () {
+        var price = this._size.price + this._stuffing.price;
+        for (var i = 0; i < this._toppings.length; i++) {
+            price += this._toppings[i].price;
+        }
+        return price;
+    };
+    Hamburger.prototype.getCalories = function () {
+        var price = this._size.cal + this._stuffing.cal;
+        for (var i = 0; i < this._toppings.length; i++) {
+            price += this._toppings[i].cal;
+        }
+        return price;
+    };
+
+
+
+var hamburger1 = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+// console.log(hamburger1.getPrice());
+hamburger1.addTopping( Hamburger.TOPPING_MAYO );
+console.log(hamburger1.getPrice());
+console.log(hamburger1.getCalories());
+
+
+
+var Calc = function(x, y){
+	this.numFirst = x,
+	this.numSecond = y
 };
 
+Calc.prototype.sum = function() {
+	return this.numFirst + this.numSecond;
+};
 
-console.log( map(f1, arr));
+var test = new Calc(10, 20);
+console.log(test.sum());
 
-
+// hamburger1.addTopping( Hamburger.TOPPING_MAYO );
